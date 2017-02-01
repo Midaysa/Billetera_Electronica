beeba47503d9686a9375f66c1164d4b6227b5ae4
Lalezka Duque <lalezkaduque@gmail.com>
2017-02-01 12:58:56 -0400

    Modificacion de clases y esqueleto de funciones
    Se modificaron los atributos de las clases y el nombre de la clase Dueño
    a Persona. Se añadió el uso de las librerias DataTime y Decimal para el
    calculo de montos y fechas.

--- ElectronicWallet.py
+++ ElectronicWallet.py
@@ -2,37 +2,49 @@
 Created on 29 ene. 2017
 
 @author: Midaysa
+@author: Lalezka
 '''
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import datatime
+import decimal
 
-class Dueño(object):
-	def __init__(self, nombre, apellido, ci, pin):
-		self.nombre = nombre
-		self.apellido = apellido
+
+class Persona(object):
+	def __init__(self, nombres, apellidos, ci, pin):
+		self.nombres = nombres
+		self.apellidos = apellidos
 		self.ci = ci
 		self.pin = pin
 
-
 class Credito(object):
-	def __init__(self, monto, fecha_trans, id_recarga):
-		self.monto = monto
-		self.fecha_trans = fecha_trans
+	def __init__(self, monto, id_recarga):
+		self.monto = decimal.Decimal(monto)
+		self.fecha_recarga = datetime.datetime.now()
 		self.id_recarga = id_recarga
 
 
 class Debito(object):
-	def __init__(self, monto, fecha_trans, id_consumo):
-		self.monto = monto
-		self.fecha_trans = fecha_trans
+	def __init__(self, monto, id_consumo):
+		self.monto = decimal.Decimal(monto)
+		self.fecha_consumo = datetime.datetime.now()
 		self.id_consumo = id_consumo
 
-
 class BilleteraElectronica(object):
-	def __init__(self, dueño):
+	def __init__(self, id, persona):
 		self.id = id
-		self.dueño = dueño
+		self.persona = persona
 		self.credito = []
 		self.debito = []
+		self.saldo = decimal.Decimal(0)
+	
+	def saldo(self):
+		return self.saldo
+	
+	def recargar(self,recarga):
+		return None
+	
+	def consumir(self,consumo):
+		return None
