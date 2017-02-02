111061644ce4d82b1e6570c689aa583814f57a01
Mida <mida.palacios@gmail.com>
2017-02-02 21:45:11 +0000

    agregados condicionales en los metodos Persona y Consumir

--- ElectronicWallet.py
+++ ElectronicWallet.py
@@ -9,28 +9,36 @@ Created on 29 ene. 2017
 # -⁻- coding: UTF-8 -*-
 # -*- coding: utf-8 -*-
 
-import datetime
+import datetime, re
 from decimal import *
 
 
 class Persona(object):
 	def __init__(self, nombres, apellidos, ci, pin):
-		try:
-			assert(type(nombres) is str)
-			assert(type(apellidos) is str)
-			assert(type(ci) is int)
-			assert(type(pin) is str)
-			
-			self.nombres = nombres
-			self.apellidos = apellidos
-			self.ci = ci
-			self.pin = pin
-		except:
-			print("Error en los datos del usuario.")
-			self.nombres = None
-			self.apellidos = None
-			self.ci = None
-			self.pin = None
+		if type(nombres) != str:
+			raise TypeError("Los nombres deben ser de tipo 'str'")
+		
+		if type(apellidos) != str:
+			raise TypeError("Los apellidos deben ser de tipo 'str")
+		
+		if type(ci) != int:
+			raise TypeError("La cedula de identidad debe ser un entero")
+		
+		if type(pin) != str:
+			raise TypeError("El PIN debe ser de tipo 'str'")
+		
+		pattern = '[^A-Za-z0-9]'
+		if re.search(pattern, nombres):
+			raise ValueError("Se ingreso un caracter especial en el nombre")
+		
+		if re.search(pattern, apellidos):
+			raise ValueError("Se ingreso un caracter especial en el apellido")
+		
+		self.nombres = nombres
+		self.apellidos = apellidos
+		self.ci = ci
+		self.pin = pin
+
 
 class Credito(object):
 	
@@ -38,6 +46,9 @@ class Credito(object):
 	
 	def __init__(self, monto, id_recarga):
 		try:
+			if type(id_recarga) != str:
+				raise TypeError("El id de la recarga debe ser de tipo string")
+			
 			assert(type(monto) is int or type(monto) is float)
 			self.monto = Decimal(monto)
 			self.fecha_recarga = datetime.datetime.now()
@@ -98,6 +109,10 @@ class BilleteraElectronica(object):
 		if (consumo.monto <= 0):
 			print("El consumo a realizar debe ser un numero natural.")
 			return -1
+		
+		if (type(consumo.monto) != int and type(consumo.monto) == float) or \
+		   (type(consumo.monto) == int and type(consumo.monto) != float):
+			raise TypeError("El monto debe ser de tipo entero o decimal")
 		
 		self.debitos.append(consumo)
 		self.saldo_actual -= consumo.monto
