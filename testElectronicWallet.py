5e0b7f0cadb7e72ed8d6f507f35dce566ab1df81
Mida <mida.palacios@gmail.com>
2017-02-02 03:37:18 +0000

    casos de prueba generales y esquina

--- testElectronicWallet.py
+++ testElectronicWallet.py
@@ -9,26 +9,57 @@ Create on 01/2/2017
 # -⁻- coding: UTF-8 -*-
 # -*- coding: utf-8 -*-
 
-import unittest
+import unittest, datetime
 from ElectronicWallet import *
 
 class billeteraElectronicaTeste(unittest.TestCase):
     
     # Caso: Crear Billetera
     def testCrearBilletera(self):
-        return
+        andrea = Persona('Andrea', 'Da Costa', 21800451, '12345')
+        andrea_billetera = BilleteraElectronica('12345', andrea)
+        
+        self.assertMultiLineEqual(andrea_billetera.persona.nombres, 'Andrea')
+        self.assertMultiLineEqual(andrea_billetera.persona.apellidos, 'Da Costa')
+        self.assertEqual(andrea_billetera.persona.ci, 21800451)
+        self.assertMultiLineEqual(andrea_billetera.persona.pin, '12345')
     
     # Caso: Recarga correcta
     def testRecargaCorrecta(self):
-        return
+        andres = Persona('Andres', 'Gonzalez', 21450451, '12345')
+        andres_billetera = BilleteraElectronica('12345', andres)        
+        nueva_recarga = Credito(150, '12500971')
+        andres_billetera.recargar(nueva_recarga)
+        
+        self.assertEqual(andres_billetera.creditos[-1].monto, 150)
+        self.assertAlmostEqual(andres_billetera.creditos[-1].id_recarga, '12500971')
     
     # Caso: Consumo correcto
     def testConsumoCorrecto(self):
-        return
-    
+        andrea = Persona('Andrea', 'Da Costa', 21800451, '12345')
+        andrea_billetera = BilleteraElectronica('12345', andrea)
+        nueva_recarga = Credito(150, '20123920')
+        nuevo_consumo = Debito(89, '30110043')
+        andrea_billetera.recargar(nueva_recarga)
+        andrea_billetera.consumir(nuevo_consumo)
+        
+        self.assertEqual(andrea_billetera.saldo(), 61)
+
     # Caso: Recargas y consumos
     def testRecargasConsumos(self):
-        return
+        andres = Persona('Andres', 'Gonzalez', 21450451, '12345')
+        andres_billetera = BilleteraElectronica('12345', andres)        
+        primera_recarga = Credito(150, '12500971')
+        andres_billetera.recargar(primera_recarga)
+        primer_consumo = Debito(46, '10101024')
+        andres_billetera.consumir(primer_consumo)
+        segunda_recarga = Credito(215, '52139021')
+        andres_billetera.recargar(segunda_recarga)
+        segundo_consumo = Debito(12, '32012311')
+        andres_billetera.consumir(segundo_consumo)
+        
+        self.assertEqual(andres_billetera.saldo(), 307)
+        
         
     # Casos esquina
     # 2.- Crear billetera con pin incorrecto
@@ -37,12 +68,18 @@ class billeteraElectronicaTeste(unittest.TestCase):
     
     # 3.- Verificacion de fechas
     def testVerificacionFecha(self):
-        return
+        nueva_recarga = Credito(120, '65014932')
+        fecha_actual = datetime.datetime.now()
+        
+        # Para la recarga
+        self.assertEqual(nueva_recarga.fecha_recarga.year, fecha_actual.year)
+        self.assertEqual(nueva_recarga.fecha_recarga.month, fecha_actual.month)
+        self.assertEqual(nueva_recarga.fecha_recarga.day, fecha_actual.day)
+        self.assertEqual(nueva_recarga.fecha_recarga.hour, fecha_actual.hour)
     
     # 4.- Recarga y consumo con decimales pequeños
     def testRecargaConsumoDecimalesPequenos(self):
-        return
-    
+        
     # 5.- Recarga y consumo negativos
     def testRecargaConsumoNegativos(self):
         return 
