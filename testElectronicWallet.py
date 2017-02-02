0293694e86cae211624585077585deabb74fd051
Mida <mida.palacios@gmail.com>
2017-02-02 00:25:46 +0000

    agregadas las pruebas de ElectronicWalletTest

--- testElectronicWallet.py
+++ testElectronicWallet.py
@@ -123,16 +123,31 @@ class billeteraElectronicaTeste(unittest.TestCase):
     
     # 4.- Consumir con pin incorrecto
     def testConsumoPinIncorrecto(self):
-        return
+        with self.assertRaises(ValueError) as context:
+            nuevo_debito = Debito(40, '900464456')
+            pedro = Persona('Pedro', 'Camejo', 1715630, '12345')
+            BilleteraElectronica('23417', pedro).consumir(nuevo_debito)
+            
+        self.assertTrue('El PIN ingresado no coincide con el del usuario' in str(context.exception))
+        
+    # 5.- Consumir con pin correcto
+    def testPinCorrecto(self):
+        nuevo_debito = Debito(40, '90013422')
+        pedro = Persona('Pedro', 'Camejo', 1715630, '12345')
+        pedro_billetera = BilleteraElectronica('12345', pedro)
+        nuevo_credito = Credito(100, '94320110')
+        pedro_billetera.recargar(nuevo_credito)
+        pedro_billetera.consumir(nuevo_debito)
+        
+        self.assertEqual(pedro_billetera.saldo(), 60)
     
-    # 5.- Recarga y consumo decimal
+    # 6.- Recarga y consumo decimal
     def testRecargaConsumoDecimal(self):
         return
     
-    # 6.- Recarga y consumo entero
+    # 7.- Recarga y consumo entero
     def testRecargaConsumoEntero(self):
         return
-
 
 if __name__ == "__main__":
     unittest.main()
